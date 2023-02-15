1d40e19a0c15725264d08e405fe9892e089123ed
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2023-02-15 14:45:23 +0700

    Add new metrics: ParaScore

--- score_parascore.py
+++ score_parascore.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import evaluate
-metrics = evaluate.load('bertscore')
+metrics = evaluate.load('transZ/test_parascore')
 
 import pandas as pd
 # df = pd.read_csv('data/new.txt', names=["Output1", "Output2", "Output3", "Output4", "Truth"])
@@ -12,8 +12,8 @@ for _, row in df.iterrows():
     truth = row['Truth']
     arr = []
     for i in range(4):
-        res = metrics.compute(predictions=[row[f"Output{i+1}"]], references=[truth], lang="en", model_type='microsoft/deberta-xlarge-mnli')
-        arr.append(res['f1'][0])
+        res = metrics.compute(predictions=[row[f"Output{i+1}"]], references=[truth], model_type='microsoft/deberta-xlarge-mnli')
+        arr.append(res['score'][0])
     max_score = max(arr)
     final_scores.append(max_score)
 
