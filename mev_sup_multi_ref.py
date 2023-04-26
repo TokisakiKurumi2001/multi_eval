b2e0c997bf98e9197a6eba9bb87bc1c32364142f
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2023-04-26 11:56:16 +0000

    Supervised multi evaluation metrics

--- mev_sup_multi_ref.py
+++ mev_sup_multi_ref.py
@@ -8,23 +8,19 @@ def average(nums: List[float]) -> float:
     return np.average(nums)
 
 if __name__ == "__main__":
-    # supervised mean input & reference
+    # supervised multi reference
 
     # metrics will be used
-    # BERTScore (input, prediction)
-    # ParaScore (input, prediction, reference)
-    # BERT-iBLEU (input, prediction)
-    # SacreBLEU (prediction, reference)
-    # TER (prediction, reference)
+    # BERTScore
+    # ParaScore
+    # BERT-iBLEU
 
     bertscore = evaluate.load('bertscore')
     parascore = evaluate.load('metrics/parascore.py')
     bert_ibleu = evaluate.load('metrics/bert_ibleu.py')
-    sacrebleu = evaluate.load('sacrebleu')
-    ter = evaluate.load('ter')
 
-    # dataset: opusparcus_input | pawsx_input
-    path = "eval_dataset/opusparcus_input"
+    # dataset: wmt19_input
+    path = "eval_dataset/wmt19_input"
     langs = os.listdir(path)
     for lang in langs:
         print(f"Lang: {lang}")
@@ -40,10 +36,9 @@ if __name__ == "__main__":
         parascore_scores = parascore.compute(
             predictions=df['prediction'],
             sources=df['input'],
-            references=df['reference'],
+            references=df['input'],
             model_type='bert-base-multilingual-cased',
             lang=lang,
-            calc_ref=True,
         )
         print(f"ParaScore: {average(parascore_scores['score'])}")
         bert_ibleu_scores = bert_ibleu.compute(
@@ -53,24 +48,5 @@ if __name__ == "__main__":
             lang=lang,
         )
         print(f"BERT-iBLEU: {average(bert_ibleu_scores['score'])}")
-        if lang == 'zh':
-            tokenizer = 'zh'
-        elif lang == 'ja':
-            tokenizer = 'ja-mecab'
-        else:
-            tokenizer = '13a'
-        sacrebleu_scores = sacrebleu.compute(
-            predictions=df['prediction'],
-            references=df['reference'],
-            tokenize=tokenizer,
-        )
-        print(f"SacreBLEU: {sacrebleu_scores['score']}")
-        ter_scores = ter.compute(
-            predictions=df['prediction'],
-            references=df['reference'],
-            normalized=True,
-            support_zh_ja_chars=True,
-        )
-        print(f"TER: {ter_scores['score']}")
 
 
